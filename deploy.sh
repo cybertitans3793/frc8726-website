fdbfa31e29258028b3055e37eb30c2b494a33b5a
cybertitans3793 <joshua.holmes@fcps.org>
2026-04-25 13:31:21 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/frc8726-website
 git pull origin main
+cd /home/ubuntu/frc-website
+docker compose down && docker compose up -d
 echo "Deployed at $(date)" >> /home/ubuntu/frc8726-website/deploy.log
