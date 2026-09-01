91c48da25d77c390fc25eaf39ff379282377e1a8
pimedia-server <pi@pimedia.local>
2026-09-01 18:33:20 -0400

    Fix deploy.sh paths for pimedia migration; gitignore blog runtime data

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/ubuntu/frc8726-website
+cd /home/pi/frc/frc8726-website
 git fetch origin main
 git reset --hard origin/main
-cd /home/ubuntu/frc-website
+cd /opt/stacks/frc-website
 docker compose down && docker compose up -d
-echo "Deployed at $(date)" >> /home/ubuntu/frc8726-website/deploy.log
+echo "Deployed at $(date)" >> /home/pi/frc/frc8726-website/deploy.log
